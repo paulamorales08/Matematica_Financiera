797eabbae45b85099a1fa2011da0700c82a220db
sebastian2909 <sebastian_2909_99@hotmail.com>
2020-05-13 09:50:02 -0500

    aumentar cuotas

--- js/main.js
+++ js/main.js
@@ -41,34 +41,34 @@ $(document).ready(function () {
       var val = valor * 12;
       console.log(val);
     }
-      var r = Math.pow(1 + valor, -plazo);
-      r = 1 - r;
-      var r2 = r / valor;
-      var r3 = capital / r2;
-      var val2 = r3.toFixed();
-      resulcuota.innerHTML = `<p>Cuota: ${val2}</p>`;
-      operacuota.innerHTML = `<p><b>Operacion</b></p>`;
-      operacuota.innerHTML += `<p><b>1.</b> 1 - (1 + ${val})^${-plazo} = ${r}</p>`;
-      operacuota.innerHTML += `<p><b>2.</b> ${r} / ${val} = ${r2}</p>`;
-      operacuota.innerHTML += `<p><b>3.</b> ${capital} / ${r2} = ${val2}</p>`;
-      var cap = capital;
-      var i = 1;
-      for (var a = 1; a <= plazo; a++) {
-        var int = (cap * val).toFixed();
-        var val3 = "";
-        var clase = "";
-        if (mesamort == a) {
-          clase = "bg-success";
-          var cuota = "Valor Amortizacion";
-          val3 = valoramort;
-        } else {
-          var cuota = "Cuota";
-          val3 = val2;
-        }
-        var amorti = (val3 - int).toFixed();
-        var saldo = cap - amorti;
-        if (saldo >= 0) {
-          var dato = `
+    var r = Math.pow(1 + valor, -plazo);
+    r = 1 - r;
+    var r2 = r / valor;
+    var r3 = capital / r2;
+    var val2 = r3.toFixed();
+    resulcuota.innerHTML = `<p>Cuota: ${val2}</p>`;
+    operacuota.innerHTML = `<p><b>Operacion</b></p>`;
+    operacuota.innerHTML += `<p><b>1.</b> 1 - (1 + ${val})^${-plazo} = ${r}</p>`;
+    operacuota.innerHTML += `<p><b>2.</b> ${r} / ${val} = ${r2}</p>`;
+    operacuota.innerHTML += `<p><b>3.</b> ${capital} / ${r2} = ${val2}</p>`;
+    var cap = capital;
+    var i = 1;
+    for (var a = 1; a <= 100; a++) {
+      var int = (cap * val).toFixed();
+      var val3 = "";
+      var clase = "";
+      if (mesamort == a) {
+        clase = "bg-success";
+        var cuota = "Valor Amortizacion";
+        val3 = valoramort;
+      } else {
+        var cuota = "Cuota";
+        val3 = val2;
+      }
+      var amorti = (val3 - int).toFixed();
+      var saldo = cap - amorti;
+      if (saldo >= 0) {
+        var dato = `
           <tr class="${clase}">
             <td>${a}</td>
             <td><a title="Deuda Actual">${cap}</a></td>
@@ -78,32 +78,39 @@ $(document).ready(function () {
             <td><a title="${cap} - ${amorti}">${saldo}</a></td>
           </tr>
           `;
-        } else if (i <= 1) {
-          i++;
-          if (i == 2) {
-            val3 = cap;
-            amorti = (val3 - int).toFixed();
-            saldo = cap - amorti;
+      } else if (i <= 1) {
+        i++;
+        if (i == 2) {
+          val3 = parseInt(cap) + parseInt(int);
+          amorti = (val3 - int).toFixed();
+          saldo = cap - amorti;
+          if (cap >= 10000) {
+            var dato = `
+              <tr>
+                <td>${a}</td>
+                <td><a title="Deuda Actual">${cap}</a></td>
+                <td><a title="${cap} / ${val}">${int}</a></td>
+                <td><a title="${val3} - ${int}">${amorti}</a></td>
+                <td><a title="${cap} + ${int}">${val3}</a></td>
+                <td><a title="${cap} - ${amorti}">${saldo}</a></td>
+              </tr>
+            `;
+          } else {
+            var dato = `
+              <tr>
+              </tr>
+            `;
           }
-          var dato = `
-          <tr>
-            <td>${a}</td>
-            <td><a title="Deuda Actual">${cap}</a></td>
-            <td><a title="${cap} / ${val}">${int}</a></td>
-            <td><a title="${val3} - ${int}">${amorti}</a></td>
-            <td><a title="${cuota}">${val3}</a></td>
-            <td><a title="${cap} - ${amorti}">${saldo}</a></td>
-          </tr>
-          `;
-        } else {
-          i++;
-          var dato = `
-          <tr>
-          </tr>
-          `;
         }
-        tabla.innerHTML += dato;
-        cap = saldo;
+      } else {
+        i++;
+        var dato = `
+          <tr>
+          </tr>
+          `;
       }
+      tabla.innerHTML += dato;
+      cap = saldo;
+    }
   });
 });
